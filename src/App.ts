178b76549ce3f0d033ed97fbf49715361478ea2f
GUILHERME FEITOSA CUNHA <126368583+guilherme273@users.noreply.github.com>
2025-02-28 03:10:56 -0300

    2
    2

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,24 @@
-import express, { Application } from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import cors from 'cors';
+import express, { Application } from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 class App {
   private app: Application;
   private http: http.Server;
   private io: Server;
+  private rooms: [] = [
+    "Falando de Java",
+    "Discução Sobre Ia",
+    "Procuro Por Um Freela",
+    "NodeJS",
+    "Cloud",
+  ];
 
   constructor() {
     this.app = express();
     this.http = http.createServer(this.app);
+
     this.io = new Server(this.http, {
       cors: {
         origin: "*", // Permite qualquer origem
@@ -24,18 +32,31 @@ class App {
       methods: ["GET", "POST"], // Permite GET e POST
       allowedHeaders: ["Content-Type"], // Permite o Content-Type
     };
-    // Configuração do CORS para o servidor Express
 
     this.app.use(cors(corsOptions));
   }
 
   listenServer() {
-    this.http.listen(3000, () => console.log('Server is running!'));
+    this.http.listen(3000, () => console.log("Server is running!"));
   }
 
   listenIo() {
-    this.io.on('connection', (socket) => {
-      console.log('Cliente conectado:', socket.id);
+    this.io.on("connection", (socket) => {
+      console.log("Cliente conectado:", socket.id);
+
+      socket.on("joinRoom", (roomName) => {
+        socket.join(roomName);
+        console.log(`${socket.id} entrou na sala: ${roomName}`);
+      });
+
+      socket.on("message", (msg) => {
+        console.log("message:", msg.msg);
+        this.io.emit("message", msg);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("Usuário desconectado:", socket.id);
+      });
     });
   }
 }
@@ -43,5 +64,3 @@ class App {
 const app = new App();
 app.listenServer();
 app.listenIo();
-
-
